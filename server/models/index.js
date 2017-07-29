d7eef3ff8705c48f9c59318d7a9caa1f6970a494
francisngo <francis.t.ngo@gmail.com>
2017-07-29 12:32:30 -0700

    add pseudocode for models

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,27 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      //create a results array/object
+      //query our database to get all messages
+      //push all messages into results array/object
+      //return array/object
+    }, // a function which produces all the messages
+    post: function (message) {
+      //insert message object inside messages table
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      //create a results array to store all user data
+      //query database to get all users
+      //push all messages in results array/object
+      //return array/object
+    },
+    post: function (user) {
+      //insert user argument/object into user table
+    }
   }
 };
-
